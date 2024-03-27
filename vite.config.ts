50dda64d703f600846b436f8d96b40ec090f40d8
taher08 <tahourawerfelli@gmail.com>
2024-03-27 20:53:43 +0100

    build update 2.05

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,8 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ command, mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), '');
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
 
-  return {
-    plugins: [react()],
-    // vite config
-    define: {
-      ...Object.keys(env).reduce((prev: Record<string, string>, key: string) => {
-        prev[`process.env.${key}`] = JSON.stringify(env[key]);
-        return prev;
-      }, {}),
-    },
-  };
-});
