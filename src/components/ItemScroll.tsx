1febc308c0a65c7a2888cca5caacab5f61800733
taher08 <tahourawerfelli@gmail.com>
2024-03-29 16:56:30 +0100

    build update 3.01

--- src/components/ItemScroll.tsx
+++ src/components/ItemScroll.tsx
@@ -22,14 +22,14 @@ export const ItemScroll: React.FC<{ children: React.ReactNode }> = ({ children }
     };
 
     return (
-        <div style={{ maxWidth: '1000px', display: 'flex', overflow: 'hidden', scrollBehavior: 'smooth', justifyContent: 'center' , alignItems: 'center'}}>
-            <button className='btn btn-circle btn-ghost' onClick={scrollLeft}>
+        <div style={{ maxWidth: '1000px', display: 'flex', overflow: 'hidden', scrollBehavior: 'smooth', justifyContent: 'center' , alignItems: 'start'}}>
+            <button className='btn btn-circle btn-ghost mt-20' onClick={scrollLeft}>
             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M15 18l-6-6 6-6"/></svg>
             </button>
             <div ref={scrollRef} style={{ display: 'flex', overflowX: 'hidden' }}>
                 {children}
             </div>
-            <button className='btn btn-circle btn-ghost' onClick={scrollRight}>
+            <button className='btn btn-circle btn-ghost mt-20' onClick={scrollRight}>
             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M9 18l6-6-6-6"/></svg>
             </button>
         </div>
