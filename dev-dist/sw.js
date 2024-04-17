485d7fbf043dfc30b87a8621b44259d928c7bd4d
taher08 <tahourawerfelli@gmail.com>
2024-04-18 01:11:36 +0200

    bup7.02

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.t61shq2u778"
+    "revision": "0.88ppha7too8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
