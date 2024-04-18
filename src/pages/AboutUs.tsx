34638a4e46e9725432c128d5b925023b708640b4
taher08 <tahourawerfelli@gmail.com>
2024-04-18 02:03:04 +0200

    bup7.02

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -17,11 +17,13 @@ export default function AboutUs(){
         <Header/>
         <div className="flex justify-center w-full my-10">
         <div className="bg-base-200 rounded-lg mx-10">
-            <h1 className='flex justify-center items-start text-4xl font-bold mt-5 '>About Us:</h1>
-            <div className='m-10 h-[30rem] text-xl'>
-                {/*<p>TUNISTREAM.CLUB your one-stop destination for streaming movies, series, anime, and more!<br></br>
-                It's a streaming platform – it's also a community.</p>*/}
-                <p className="font-bold mt-10">- This site does not upload/host any of the streamed media.</p>
+            <h1 className='flex justify-center items-start text-4xl font-bold mt-5 '>About us:</h1>
+            <div className='m-10 h-[30rem] text-left text-base'>
+                <p>TUNISTREAM.CLUB is a platform for streaming TV shows and movies <br></br>
+                Uses The Movie Database <span  className="font-semibold italic">(TMDB)</span> for media information.<br></br>
+                And works by searching the web for streams through various sources!</p>
+                <p className="font-bold mt-10">- This site does not upload/host any of the streamed media.<br></br>
+                There for has no control over whether the media is available or not. </p>
             </div>
         </div>
         </div>
--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -41,12 +41,9 @@ const Settings: React.FC = () => {
                 <div className="divider my-2 h-1"></div> 
               
                   <div className="dropdown mb-10">
-                  <div tabIndex={0} role="button" className="btn btn-wide border-2 border-gray-600 text-xl m-1">
-
-
-
-                    Select Language
-                    <svg width="12px" height="12px" className="h-2 w-2 fill-current opacity-60 inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2048 2048"><path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path></svg>
+                  <div tabIndex={0} role="button" className="btn btn-wide border-2 flex justify-between border-gray-600 text-xl m-1">
+                    <span>Select Language</span>
+                    <svg width="20px" height="20px" className="h-3 w-3 fill-current opacity-60 inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2048 2048"><path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path></svg>
                   </div>
 
                   <ul tabIndex={0} className="dropdown-content z-[1] p-2 shadow-2xl bg-base-300 rounded-box w-52">
