6acd6e04ea2768fec4bee855d14328beac4eb0b8
taher08 <tahourawerfelli@gmail.com>
2024-04-20 18:14:27 +0200

    bup7.8

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -732,11 +732,11 @@ const handleSettings = ()=>{
 
 
         <div className={`absolute w-full h-fit bottom-0 pb-3 sm:pb-0 rounded-lg transition-all duration-300 ${showUI ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-5'} z-50`}>
-        <div className="relative  w-[80vw] mx-[10vw] sm:w-[95vw] sm:mx-[2.5vw] sm:-my-[8px]">
+        <div className="relative  w-[80vw] mx-[10vw] sm:w-[97vw] sm:mx-[1.5vw] sm:-my-[8px]">
         <RangeSlider Value={videoLoaded ? progress : 0} BufferValue={Math.round(loadedFraction * 10000)} onChange={(value) => handleProgress(value)}/>
         </div>
-        <div className='flex flex-start justify-between items-center  w-[80vw] mx-[10vw] sm:w-[95vw] sm:mx-[2.5vw] sm:-my-[8px]'>
-        <div className='flex flex-row items-center  w-fit mb-3 '>
+        <div className='flex flex-start justify-between items-center  w-[80vw] mx-[10vw] sm:w-[97vw] sm:mx-[1.5vw] sm:-my-[8px]'>
+        <div className='flex flex-row items-center  w-fit mb-4 '>
         <button className='btn btn-ghost px-2' onClick={togglePlayback}>
         {playing ? 
        <svg xmlns="http://www.w3.org/2000/svg" width="29" height="29" viewBox="0 0 24 24" fill="white" stroke="white" stroke-width="0.5" stroke-linecap="round" stroke-linejoin="round"><rect x="5" y="4" width="5" height="16"></rect><rect x="14" y="4" width="5" height="16"></rect></svg>
@@ -775,7 +775,7 @@ const handleSettings = ()=>{
       <h2 className='select-text text-[0.9rem] sm:text-[1.15rem]  text-gray-300 ml-1'>{formatTime(currentTime)} / {formatTime(duration)}</h2>
       </div>
 
-        <div className='flex flex-row  mb-3'>
+        <div className='flex flex-row mb-4'>
           <button className="btn btn-ghost px-3" onClick={handleSettings}>
           <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" className={` transition-transform duration-200 ${settings ? 'rotate-[45deg]':'null'}`} viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="1.5" stroke-linecap="butt" stroke-linejoin="bevel"><circle cx="12" cy="12" r="3"></circle><path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path></svg>
           </button>
